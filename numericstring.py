6a36546d0679d9b26195689159b2526dc668c96c
Pradeep Gupta <pg7832649@gmail.com>
2017-05-29 13:16:15 +0530

    Create numericstring.py
    Used to sort a big numeric string list in python

---
+++ numericstring.py
@@ -0,0 +1,12 @@
+import sys
+
+
+n = int(input().strip())
+unsorted = []
+unsorted_i = 0
+for unsorted_i in range(n):
+   unsorted_t = str(input().strip())
+   unsorted.append(unsorted_t)
+unsorted.sort(key=int)
+for i in range(n):
+   print(unsorted[i])
